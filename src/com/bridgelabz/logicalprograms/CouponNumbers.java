50441aca6ab83a607c9ab2701c6c42af989220d1
padmini v <padmini.v@perfios.com>
2021-08-24 20:42:43 +0530

    MODIFIED:Added iterations to generate random numbers

--- src/com/bridgelabz/logicalprograms/CouponNumbers.java
+++ src/com/bridgelabz/logicalprograms/CouponNumbers.java
@@ -1,17 +1,19 @@
 package com.bridgelabz.logicalprograms;
 
 import java.util.Scanner;
-public class CouponNumbers {
+public class CouponNumbers 
+{
 
-	public static void main(String[] args) {
-		Scanner object=new Scanner(System.in);
+	public static void main(String[] args) 
+	{
+		Scanner scanner=new Scanner(System.in);
 		System.out.println("Enter the N value");
-		int numberOfCoupons=object.nextInt();
+		int numberOfCoupons=scanner.nextInt();
 		int couponNumbers[]=new int[numberOfCoupons];
 		System.out.println("Enter "+numberOfCoupons+" distinct coupon numbers");
 		for(int index=0;index<numberOfCoupons;index++)
 		{
-			couponNumbers[index]=object.nextInt();
+			couponNumbers[index]=scanner.nextInt();
 			
 		}
 		randomNumbers(couponNumbers);
@@ -20,10 +22,24 @@ public class CouponNumbers {
 	
 	}
 
-	private static void randomNumbers(int couponNumbers[]) {
-		
-		
-		
+	private static void randomNumbers(int couponNumbers[]) 
+	{
+		int count=0;
+		int iterations=0;
+		while(count!=couponNumbers.length)
+		{
+			int randomNumber=(int)(Math.random()*9) + 1;
+			for(int index=0;index<couponNumbers.length;index++)
+			{
+				if(randomNumber==couponNumbers[index]) {
+					count++;
+					couponNumbers[index]=-1;
+				}
+			}
+			iterations++;
+			
+		}
+		System.out.println(iterations);
 		
 	}
 
